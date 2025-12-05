58cf3ecb1274839811e96209754d1656ebbf6093
ilyasin001 <ilymuuh@gmail.com>
2025-12-05 17:14:53 +0000

    +comments

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -4,43 +4,49 @@ import "./App.css";
 import { useState } from "react";
 
 const api = {
-  key: "ae657a09ca34013154f5e061ac26a02d",
-  geo: "https://api.openweathermap.org/geo/1.0/",
-  base: "https://api.openweathermap.org/data/2.5/",
+  key: "ae657a09ca34013154f5e061ac26a02d", // API KEY
+  geo: "https://api.openweathermap.org/geo/1.0/", // GEO API
+  base: "https://api.openweathermap.org/data/2.5/", // WEATHER API
 };
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState(null);
 
+  const [search, setSearch] = useState(""); // get and set the user input
+  const [weather, setWeather] = useState(null); // get and set the data
+
+  // async func to wait for full search
   const searchPressed = async () => {
     if (!search) return alert("Please enter a city.");
 
     try {
+      // fetches geo api response
       const geoRes = await fetch(
         `${api.geo}direct?q=${encodeURIComponent(
           search
         )}&limit=1&appid=${api.key}`
       );
+      // convert retrieved data into readable json format
       const geoData = await geoRes.json();
-
+      // if geo returns null or returns nothing then alert 
       if (!geoData || geoData.length === 0) {
         alert("City not found. Please enter a valid city.");
         setWeather(null);
         return;
       }
-
+      // reading and storing the data from json file 
       const { lat, lon, name, country } = geoData[0];
-
+      // double check with fetch to ensure city is valid
       const weatherRes = await fetch(
         `${api.base}weather?lat=${lat}&lon=${lon}&units=metric&appid=${api.key}`
       );
+      // convert retrieved data into readable json format
       const weatherData = await weatherRes.json();
 
       weatherData.cityName = name;
       weatherData.country = country;
 
       setWeather(weatherData);
+      // error handling
     } catch (error) {
       alert("An error occurred. Try again.");
       console.error(error);
